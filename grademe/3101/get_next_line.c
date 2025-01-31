a931e15ed82b97acde2d136fae08caf2577bf0c0
CoJoRi <rinaudo.joseph12@gmail.com>
2025-01-31 09:54:57 +0100

    training for gnl

--- grademe/3101/get_next_line.c
+++ grademe/3101/get_next_line.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrinaudo <jrinaudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/30 20:33:59 by jrinaudo          #+#    #+#             */
-/*   Updated: 2025/01/30 22:34:33 by jrinaudo         ###   ########.fr       */
+/*   Created: 2025/01/31 09:01:14 by jrinaudo          #+#    #+#             */
+/*   Updated: 2025/01/31 09:52:37 by jrinaudo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,20 @@
 #endif
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-char	*ft_strdup(char * str)
+char *dupli(char *str)
 {
-	int	i = 0;
-	char *cpy = NULL;
+	int i = 0;
+	char *cpy;
 
 	if (!str)
 		return (NULL);
 	while (str[i])
 		i++;
-	cpy = malloc(i + 1);
-	if (!cpy)
-		return (NULL);
+	cpy = malloc (i + 1);
 	i = 0;
 	while (str[i])
 	{
@@ -37,43 +36,43 @@ char	*ft_strdup(char * str)
 		i++;
 	}
 	cpy[i] = '\0';
-	return (cpy);	
+	return (cpy);
 }
-
-
 char *get_next_line(int fd)
 {
-	static char	buff[BUFFER_SIZE + 1] = {0};
+	static char buff[BUFFER_SIZE +1];
 	static int	index_buff = 0;
 	static int	nbchar = 0;
+	char		line[1000000] = {'\0'};
 	int			index_line = 0;
-	char		line[1000000] = {0};
 
-	if (BUFFER_SIZE < 0 || fd < 0)
+	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
 	while (1)
 	{
 		if (index_buff >= nbchar)
 		{
-			nbchar = read(fd, buff, BUFFER_SIZE);
 			index_buff = 0;
+			nbchar = read (fd, buff, BUFFER_SIZE);
 			if (nbchar <= 0)
 				break;
 		}
 		line[index_line++] = buff[index_buff++];
-		if (line[index_line -1] == '\n')
+		if (line[index_line - 1] == '\n')
 			break;
 	}
 	if (index_line == 0)
 		return (NULL);
-	return (ft_strdup(line));
+	if (nbchar < 0)
+		return (NULL);
+	return (dupli(line));
 }
 
-#include <stdio.h>
+/* #include <stdio.h>
 int main(void)
 {
 	char *line;
-	int fd = open("1", O_RDONLY);
+	int fd = open("5", O_RDONLY);
 
 	while ((line = get_next_line(fd)) != NULL)
 	{
@@ -82,4 +81,8 @@ int main(void)
 		line = NULL;
 	}
 	close(fd);
-}
+} */
+/* 
+line[index_line-1]
+buff[index_buff-1]
+ */
